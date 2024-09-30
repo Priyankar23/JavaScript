10688c40955eeec749007b8ba061a590279ed0d7
Priyanka R <priyacs099@gmail.com>
2024-09-30 17:32:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function togglesection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section.style.display === "block") {
+        section.style.display = "none";
+    } else {
+        section.style.display = "block";
+    }
+}
+
+function toggleSection(sections) {
+    var section = document.getElementById(sections);
+    if (section.style.display === "block") {
+        section.style.display = "none";
+    } else {
+        section.style.display = "block";
+    }
+}
